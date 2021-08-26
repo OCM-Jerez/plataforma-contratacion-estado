e39cdbef97429c84d61331c5c2b26c0e8096920f
mammam <correomam@gmail.com>
2021-08-26 20:44:00 +0200

    Cambios Jimmy

--- src/app/pages/tables/por-licitacion/por-licitacion.component.ts
+++ src/app/pages/tables/por-licitacion/por-licitacion.component.ts
@@ -24,109 +24,161 @@ export class PorLicitacionComponent {
 	public columnDefs: any;
 	public defaultColDef: any;
 	public gridOptions: GridOptions;
+
 	public groupHeaderHeight = 25;
 	public headerHeight = 25;
 	public localeText;
 	public rowData: ILicitacion[] = [];
 	public isExpanded = false;
+	detailCellRendererParams: any;
 
 	constructor() {
+		// this.columnDefs = [
+		// 	{
+		// 		headerName: 'ID',
+		// 		field: 'ContractFolderID',
+		// 		width: 110,
+		// 		type: 'rightAligned',
+		// 		resizable: true,
+		// 	},
+		// 	{
+		// 		headerName: 'Fecha',
+		// 		field: 'updated',
+		// 		width: 90,
+		// 		resizable: true,
+		// 		valueFormatter: (params: any) => {
+		// 			return moment(moment(params.data.updated).toDate()).format('DD-MM-YYYY')
+		// 		}
+		// 	},
+		// 	{
+		// 		headerName: 'Descripción',
+		// 		field: 'Name',
+		// 		width: 900,
+		// 		resizable: true,
+		// 		wrapText: true,
+		// 		autoHeight: true,
+		// 	},
+		// 	{
+		// 		headerName: 'Adjudicatario',
+		// 		field: 'arrayTenderResult',
+		// 		width: 300,
+		// 		resizable: true,
+		// 		wrapText: true,
+		// 		autoHeight: true,
+		// 		valueFormatter: (params: any) => {
+		// 			if (params.data && params.data.arrayTenderResult) {
+		// 				const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
+		// 				const empresas = tenderResult.map((item) => {
+		// 					return `${item.PartyName} ${item.PayableAmount}`;
+		// 				});
+		// 				return empresas;
+		// 			} else {
+		// 				return null;
+		// 			}
+		// 		}
+		// 	},
+		// 	{
+		// 		headerName: 'CIF',
+		// 		field: 'PartyIdentification',
+		// 		width: 100,
+		// 		resizable: true,
+		// 		wrapText: true,
+		// 		autoHeight: true,
+		// 		valueFormatter: (params: any) => {
+		// 			if (params.data && params.data.arrayTenderResult) {
+		// 				const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
+		// 				const empresas = tenderResult.map((item) => {
+		// 					return item.PartyIdentification;
+		// 				});
+		// 				return empresas;
+		// 			} else {
+		// 				return null;
+		// 			}
+		// 		}
+		// 	},
+		// 	{
+		// 		headerName: 'Importe',
+		// 		field: 'TotalAmount',
+		// 		width: 80,
+		// 		resizable: true,
+		// 		aggFunc: 'sum',
+		// 		cellRenderer: CellRendererOCM,
+		// 		valueFormatter: (params: any) => {
+		// 			if (params.data && params.data.arrayTenderResult) {
+		// 				const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
+		// 				const empresas = tenderResult.map((item) => {
+		// 					return item.PayableAmount;
+		// 				});
+		// 				return empresas;
+		// 			} else {
+		// 				return null;
+		// 			}
+		// 		}
+		// 	}
+		// ];
+
+		// this.defaultColDef = {
+		// 	sortable: true,
+		// 	resizable: true,
+		// 	filter: true
+		// };
+
+		this.gridOptions = {} as GridOptions;
+		this.localeText = localeTextESPes;
+
+
+		this.defaultColDef = { flex: 1 };
+
 		this.columnDefs = [
-			{
-				headerName: 'ID',
-				field: 'ContractFolderID',
-				width: 110,
-				type: 'rightAligned',
-				resizable: true,
-			},
-			{
-				headerName: 'Fecha',
-				field: 'updated',
-				width: 90,
-				resizable: true,
-				valueFormatter: (params: any) => {
-					return moment(moment(params.data.updated).toDate()).format('DD-MM-YYYY')
-				}
-			},
 			{
 				headerName: 'Descripción',
 				field: 'Name',
-				width: 900,
-				resizable: true,
-				wrapText: true,
-				autoHeight: true,
+				cellRenderer: 'agGroupCellRenderer',
 			},
-			{
-				headerName: 'Adjudicatario',
-				field: 'arrayTenderResult',
-				width: 300,
-				resizable: true,
-				wrapText: true,
-				autoHeight: true,
-				valueFormatter: (params: any) => {
-					if (params.data && params.data.arrayTenderResult) {
-						const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
-						const empresas = tenderResult.map((item) => {
-							return `${item.PartyName} ${item.PayableAmount}`;
-						});
-						return empresas;
-					} else {
-						return null;
-					}
-				}
-			},
-			{
-				headerName: 'CIF',
-				field: 'PartyIdentification',
-				width: 100,
-				resizable: true,
-				wrapText: true,
-				autoHeight: true,
-				valueFormatter: (params: any) => {
-					if (params.data && params.data.arrayTenderResult) {
-						const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
-						const empresas = tenderResult.map((item) => {
-							return item.PartyIdentification;
-						});
-						return empresas;
-					} else {
-						return null;
-					}
-				}
-			},
-			{
-				headerName: 'Importe',
-				field: 'TotalAmount',
-				width: 80,
-				resizable: true,
-				aggFunc: 'sum',
-				cellRenderer: CellRendererOCM,
-				valueFormatter: (params: any) => {
-					if (params.data && params.data.arrayTenderResult) {
-						const tenderResult: IArrayTenderResult[] = params.data.arrayTenderResult;
-						const empresas = tenderResult.map((item) => {
-							return item.PayableAmount;
-						});
-						return empresas;
-					} else {
-						return null;
-					}
-				}
-			}
+			{ field: 'title' },
+			{ field: 'updated' },
+			{ field: 'ContractFolderID' },
+			{ field: 'TotalAmount' },
+
+
 		];
 
-		this.defaultColDef = {
-			sortable: true,
-			resizable: true,
-			filter: true
+		this.detailCellRendererParams = {
+			detailGridOptions: {
+				rowSelection: 'multiple',
+				suppressRowClickSelection: true,
+				enableRangeSelection: true,
+				pagination: false,
+				paginationAutoPageSize: false,
+				columnDefs: [
+					{
+						field: 'PartyName',
+					},
+					{ field: 'PartyIdentification' },
+					{ field: 'PayableAmount' }
+				],
+				defaultColDef: {
+					sortable: true,
+					flex: 1,
+				},
+			},
+			getDetailRowData: function (params: any) {
+				params.successCallback(params.data.arrayTenderResult);
+			},
 		};
-		this.gridOptions = {} as GridOptions;
-		this.localeText = localeTextESPes;
+
+	}
+
+	onFirstDataRendered(params: any) {
+		setTimeout(function () {
+			params.api.getDisplayedRowAtIndex(1).setExpanded(true);
+		}, 0);
 	}
 
 	onGridReady(params: { api: any; columnApi: any }) {
 		this.gridApi = params.api;
 		this.gridColumnApi = params.columnApi;
+
 		const data = localStorage.getItem('dataLicitacion');
 		this.rowData = JSON.parse(data!) as ILicitacion[];
 	}
