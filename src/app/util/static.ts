dc46fab02b5d260d621fb143590ca3f2aead7683
mamjerez <correomam@gmail.com>
2021-09-05 11:38:42 +0200

    Sort static

--- src/app/util/static.ts
+++ src/app/util/static.ts
@@ -1,4 +1,21 @@
 export class Static {
+	public static RANGO_IMPORTE = [
+		{ id: 1, value: { rangoInicial: 0, rangoFinal: 99, codeText: '0 - 100' } },
+		{ id: 2, value: { rangoInicial: 100, rangoFinal: 499, codeText: '100 - 500' } },
+		{ id: 3, value: { rangoInicial: 500, rangoFinal: 999, codeText: '500 - 1.000' } },
+		{ id: 4, value: { rangoInicial: 1000, rangoFinal: 1999, codeText: '1.000 - 2.000' } },
+		{ id: 5, value: { rangoInicial: 2000, rangoFinal: 2999, codeText: '2.000 - 3.000' } },
+		{ id: 6, value: { rangoInicial: 3000, rangoFinal: 3999, codeText: '3.000 - 4.000' } },
+		{ id: 7, value: { rangoInicial: 4000, rangoFinal: 4999, codeText: '4.000 - 5.000' } },
+		{ id: 8, value: { rangoInicial: 5000, rangoFinal: 9999, codeText: '5.000 - 10.000' } },
+		{ id: 9, value: { rangoInicial: 10000, rangoFinal: 13999, codeText: '10.000 - 14.000' } },
+		{ id: 10, value: { rangoInicial: 14000, rangoFinal: 14999, codeText: '14.000 - 15.000' } },
+		{ id: 11, value: { rangoInicial: 15000, rangoFinal: 24999, codeText: '15.000 - 25.000' } },
+		{ id: 12, value: { rangoInicial: 25000, rangoFinal: 34999, codeText: '25.000 - 35.000' } },
+		{ id: 13, value: { rangoInicial: 35000, rangoFinal: 44999, codeText: '35.000 - 45.000' } },
+		{ id: 14, value: { rangoInicial: 45000, rangoFinal: 1000000000, codeText: 'Más de 45.000' } }
+	];
+
 	public static TIPOS_PROCEDURE = [
 		{ id: 1, value: 'Abierto' },
 		{ id: 2, value: 'Restringido' },
@@ -19,23 +36,6 @@ export class Static {
 		{ id: 1, value: 'Ordinaria' },
 		{ id: 2, value: 'Urgente' },
 		{ id: 3, value: 'Emergencia' }
-	];
-
-	public static RANGO_IMPORTE = [
-		{ id: 1, value: { rangoInicial: 0, rangoFinal: 99, codeText: '0 - 100' } },
-		{ id: 2, value: { rangoInicial: 100, rangoFinal: 499, codeText: '100 - 500' } },
-		{ id: 3, value: { rangoInicial: 500, rangoFinal: 999, codeText: '500 - 1.000' } },
-		{ id: 4, value: { rangoInicial: 1000, rangoFinal: 1999, codeText: '1.000 - 2.000' } },
-		{ id: 5, value: { rangoInicial: 2000, rangoFinal: 2999, codeText: '2.000 - 3.000' } },
-		{ id: 6, value: { rangoInicial: 3000, rangoFinal: 3999, codeText: '3.000 - 4.000' } },
-		{ id: 7, value: { rangoInicial: 4000, rangoFinal: 4999, codeText: '4.000 - 5.000' } },
-		{ id: 8, value: { rangoInicial: 5000, rangoFinal: 9999, codeText: '5.000 - 10.000' } },
-		{ id: 9, value: { rangoInicial: 10000, rangoFinal: 13999, codeText: '10.000 - 14.000' } },
-		{ id: 10, value: { rangoInicial: 14000, rangoFinal: 14999, codeText: '14.000 - 15.000' } },
-		{ id: 11, value: { rangoInicial: 15000, rangoFinal: 24999, codeText: '15.000 - 25.000' } },
-		{ id: 12, value: { rangoInicial: 25000, rangoFinal: 34999, codeText: '25.000 - 35.000' } },
-		{ id: 13, value: { rangoInicial: 35000, rangoFinal: 44999, codeText: '35.000 - 45.000' } },
-		{ id: 14, value: { rangoInicial: 45000, rangoFinal: 1000000000, codeText: 'Más de 45.000' } }
 	];
 
 	public static TIPOS_TYPE = [
