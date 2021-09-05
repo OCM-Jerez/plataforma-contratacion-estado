9853ec6c8f86c79c4dda2d59083b520007b39414
mamjerez <correomam@gmail.com>
2021-09-05 19:43:55 +0200

    Delete IContratoMenor

--- src/app/models/contratos.interfaces.ts
+++ src/app/models/contratos.interfaces.ts
@@ -1,28 +1,5 @@
 import { TipoGrafico } from './tipos-graficos.type';
 
-export interface IContratoMenor {
-	link: string;
-	summary: string;
-	title: string;
-	updated: string;
-	ContractFolderID: string;
-	ContractFolderStatusCode: string;
-	Name: string;
-	TypeCode: string;
-	SubTypeCode: string;
-	TotalAmount: number;
-	TaxExclusiveAmount: number;
-	ResultCode: string;
-	AwardDate: string;
-	ReceivedTenderQuantity: string;
-	PartyIdentification: string;
-	PartyName: string;
-	// TaxExclusiveAmount1: number;
-	PayableAmount: number;
-	ProcedureCode: string;
-	UrgencyCode: string;
-}
-
 export interface IChartContrato {
 	codeText: string;
 	contratos: number;
@@ -51,6 +28,8 @@ export interface ILicitacion {
 	TaxExclusiveAmount: number;
 	DurationMeasure: string;
 	unitCode: string;
+	ProcedureCode: string;
+	UrgencyCode: string;
 	arrayTenderResult: IArrayTenderResult[];
 }
 
