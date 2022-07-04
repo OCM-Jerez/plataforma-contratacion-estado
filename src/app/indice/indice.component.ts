ac99e2e5f84d74660b5d6318c55e04471e9130b1
mamjerez <correomam@gmail.com>
2022-07-04 12:15:18 +0200

    Mes 06-2022

--- src/app/indice/indice.component.ts
+++ src/app/indice/indice.component.ts
@@ -12,7 +12,7 @@ import { ENTES_CONTRATACION } from '../../assets/data/entesContratacion-data';
 import { IEntesContratacion } from '../models/entesContratacion.interface';
 import { Static } from '../util/static';
 
-import contratosYlicitacionesJSON from '../../assets/data/todo042022NoRepeatOkCIFOK.json';
+import contratosYlicitacionesJSON from '../../assets/data/todo062022NoRepeatOkCIFOK.json';
 
 @Component({
 	selector: 'app-indice',
