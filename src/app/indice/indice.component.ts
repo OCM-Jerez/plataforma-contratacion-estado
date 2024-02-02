b84142787842a7528d9cc25455fd27ea9d8993ea
mamjerez <correomam@gmail.com>
2024-02-02 19:10:00 +0100

    Ver 1.02.02

--- src/app/indice/indice.component.ts
+++ src/app/indice/indice.component.ts
@@ -11,7 +11,7 @@ import { IEntesContratacion } from '../models/entesContratacion.interface';
 import { ChannelFilterDateService } from '../services/channel-filter-date.service';
 import { Static } from '../util/static';
 
-import contratosYlicitacionesJSON from '../../assets/data/todo122023NoRepeatOkCIFOK.json';
+import contratosYlicitacionesJSON from '../../assets/data/todo012024NoRepeatOkCIFOK.json';
 
 @Component({
 	selector: 'app-indice',
